f9295222ceed71badae264450d08dbb9d319f3d6
Rushabh Narekar <rushabh4gvm@gmail.com>
2023-02-03 11:20:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+require('dotenv').config()
+const { Configuration, OpenAIApi } = require("openai");
+const cors = require("cors");
+//express
+const app = express()
+app.use(express.json())
+//
+
+app.use(cors())
+
+//ChatGPT Configuration
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+//
+
+//api
+app.post("/ChatWithGvm",async(req,res)=>{
+    try{
+        //`${prompt}`,
+        // debugger
+        const {prompt}=req.body;
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0.7,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+        
+        return res.status(200).json({
+            StatusMessage:"success",
+            data:completion.data.choices[0].text
+        });
+    }catch(err){
+        return res.status(400).json({
+            ErrorMessage:"Fail",
+        });
+    }
+})
+//
+
+
+//Port listen
+const Port =process.env.PORT ||4000
+
+app.listen(Port,()=>console.log(`Server on port ${Port}`))
+//
